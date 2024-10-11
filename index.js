92736ba4bfc8b1acda60a727dbbebe664e245ad1
Jairo Litman <130161309+jairo-litman@users.noreply.github.com>
2024-10-11 20:32:25 -0300

    hero class

--- index.js
+++ index.js
@@ -1,37 +1,28 @@
-function getWinsLoses() {
-    while (true) {
-        const wins = parseInt(prompt("Insira sua quantidade de vitórias: "), 10);
-        const loses = parseInt(prompt("Insira sua quantidade de derrotas: "), 10);
-        if (!isNaN(wins) && !isNaN(loses)) {
-            return { wins, loses };
+class Hero {
+    constructor(name, level, type) {
+        this.name = name;
+        this.level = level;
+        this.type = type;
+    }
+
+    attack() {
+        let attackType;
+
+        switch (this.type) {
+            case 'mago':
+                attackType = 'magia';
+                break;
+            case 'guerreiro':
+                attackType = 'espada';
+                break;
+            case 'monge':
+                attackType = 'artes marciais';
+                break;
+            case 'ninja':
+                attackType = 'shuriken';
+                break;
         }
-        alert("Vitórias e derrotas inválidas, utilize números.");
+
+        return `o ${this.type} atacou usando ${attackType}`;
     }
 }
-
-function determineRank(wins, loses) {
-    const winRate = wins - loses;
-
-    if (winRate <= 10) {
-        return "Ferro";
-    } else if (winRate <= 20) {
-        return "Bronze";
-    } else if (winRate <= 50) {
-        return "Prata";
-    } else if (winRate <= 80) {
-        return "Ouro";
-    } else if (winRate <= 90) {
-        return "Diamante";
-    } else if (winRate <= 100) {
-        return "Lendário";
-    } else {
-        return "Imortal";
-    }
-}
-
-const { wins, loses } = getWinsLoses();
-const rank = determineRank(wins, loses);
-const winRate = wins - loses;
-
-
-console.log(`O Herói tem de saldo de ${winRate} está no nível de ${rank}`);
